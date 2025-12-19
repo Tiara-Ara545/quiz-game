c94e6594af2f71088d22d5833799264768565991
Edonis Dushica <edushica06@gmail.com>
2025-12-19 16:06:49 +0100

    projekti i quiz game

---
+++ Quiz game/script.js
@@ -0,0 +1,15 @@
+let questions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Berlin", "Madrid", "Paris", "Rome"],
+    }
+
+,{
+        question
+}];
+let currentQuestionIndex = 0;
+let score = 0;
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices");
+const scoreElement = document.getElementById("score");
+const nextButton = document.getElementById("next");
